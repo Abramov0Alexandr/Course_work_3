01aea827e6cc92203f8d5bfcbab387517dbf217e
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-02-22 22:46:32 +0300

    Написал функцию sort_data_by_time

--- utils/functions.py
+++ utils/functions.py
@@ -8,3 +8,15 @@ def json_reader() -> list:
     response = requests.get('https://api.npoint.io/27390f0e02c8539a928e')
     result = response.json()
     return result
+
+
+def sort_data_by_time(data_dict: list) -> list:
+
+    pre_sorted_data = data_dict
+
+    try:
+        sorted_data = sorted(pre_sorted_data, key=itemgetter('date'), reverse=True)
+        return sorted_data
+
+    except KeyError:
+        print('Ошибка прочтения даты в массиве данных')
