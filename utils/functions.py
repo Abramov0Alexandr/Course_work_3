4a9301067385f39635b18aa0232425da5abaf2b4
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-02-22 22:49:07 +0300

    Написал функцию users_account_editor

--- utils/functions.py
+++ utils/functions.py
@@ -34,3 +34,36 @@ def date_editor(data_dict: dict) -> str:
         formatted_date = 'Ошибка прочтения даты'
 
     return formatted_date
+
+
+def users_account_editor(data_dict: dict) -> str:
+    spaces_idx = [4, 9, 14, 19]
+    source = data_dict.get('from')
+
+    if source is not None:
+
+        if len(''.join(list(source.split()[1]))) == 20:
+            name_card = source.split()[0]
+            account_number = list(source.split()[1])
+            account_number[6:16] = '**********'
+
+        elif len(source.split(' ')) == 2:
+            name_card = source.split()[0]
+            account_number = list(source.split()[1])
+
+        elif len(source.split(' ')) == 3:
+            name_card = ' '.join(source.split()[0:2])
+            account_number = list(source.split(' ')[2])
+
+        account_number[6:12] = '******'
+
+        while spaces_idx:
+            account_number.insert(
+                spaces_idx.pop(0), ' ')
+        coded_account = ''.join(account_number)
+
+    else:
+        name_card = 'Вклад'
+        coded_account = 'открыт'
+
+    return f"{name_card} {coded_account}"
