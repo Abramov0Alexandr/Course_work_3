f745c881cd2b20d99213bf83ef41a5d401222ad5
Alexandr Abramov <alexandr.abramovvv@yandex.ru>
2023-02-26 11:39:52 +0300

    Скорректировал имя переменной в тестах

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -10,65 +10,65 @@ def test_json_reader():
 def test_empty_json_dict():
 
     testing_data = [{}, {1: 'a', 2: 'b', 3: 'c'}]
-    data_after_test = [{1: 'a', 2: 'b', 3: 'c'}]
+    desired_result = [{1: 'a', 2: 'b', 3: 'c'}]
 
-    assert functions.check_data_for_empty_dict(testing_data) == data_after_test
+    assert functions.check_data_for_empty_dict(testing_data) == desired_result
 
 
 def test_sort_by_time():
 
     testing_data = [{'date': '2017.05.11.'}, {'date': '2019.04.05.'}, {'date': '2016.01.04'}]
-    data_after_test = [{'date': '2019.04.05.'}, {'date': '2017.05.11.'}, {'date': '2016.01.04'}]
+    desired_result = [{'date': '2019.04.05.'}, {'date': '2017.05.11.'}, {'date': '2016.01.04'}]
 
-    assert functions.sort_data_by_time(testing_data) == data_after_test
+    assert functions.sort_data_by_time(testing_data) == desired_result
 
 
 def test_date_editor_normal():
 
     testing_data = {'date': '2019-12-08T22:46:21.935582'}
-    data_after_test = '08.12.2019'
+    desired_result = '08.12.2019'
 
-    assert functions.date_editor(testing_data) == data_after_test
+    assert functions.date_editor(testing_data) == desired_result
 
 
 def test_date_editor_exception():
 
     testing_data = {}
-    data_after_test = 'Ошибка прочтения даты'
+    desired_result = 'Ошибка прочтения даты'
 
-    assert functions.date_editor(testing_data) == data_after_test
+    assert functions.date_editor(testing_data) == desired_result
 
 
 def test_user_account_number():
 
     testing_data = {'from': 'Счет 49304996510329747621'}
-    data_after_test = 'Счет 4930 49** **** **** 7621'
+    desired_result = 'Счет 4930 49** **** **** 7621'
 
-    assert functions.users_account_editor(testing_data) == data_after_test
+    assert functions.users_account_editor(testing_data) == desired_result
 
 
 def test_users_account_short_name_card():
 
     testing_data = {'from': 'МИР 5211277418228469'}
-    data_after_test = 'МИР 5211 27** **** 8469'
+    desired_result = 'МИР 5211 27** **** 8469'
 
-    assert functions.users_account_editor(testing_data) == data_after_test
+    assert functions.users_account_editor(testing_data) == desired_result
 
 
 def test_users_account_long_name_card():
 
     testing_data = {'from': 'Visa Classic 6831982476737658'}
-    data_after_test = 'Visa Classic 6831 98** **** 7658'
+    desired_result = 'Visa Classic 6831 98** **** 7658'
 
-    assert functions.users_account_editor(testing_data) == data_after_test
+    assert functions.users_account_editor(testing_data) == desired_result
 
 
 def test_users_account_empty_source():
 
     testing_data = {'from': None}
-    data_after_test = 'Вклад открыт'
+    desired_result = 'Вклад открыт'
 
-    assert functions.users_account_editor(testing_data) == data_after_test
+    assert functions.users_account_editor(testing_data) == desired_result
 
 
 def test_beneficiary_account_editor_exceptions():
@@ -80,14 +80,14 @@ def test_beneficiary_account_editor_exceptions():
 def test_beneficiary_short_name_card():
 
     testing_data = {'to': 'Maestro 3806652527413662'}
-    data_after_test = 'Maestro **3662'
+    desired_result = 'Maestro **3662'
 
-    assert functions.beneficiary_account_editor(testing_data) == data_after_test
+    assert functions.beneficiary_account_editor(testing_data) == desired_result
 
 
 def test_beneficiary_long_name_card():
 
     testing_data = {'to': 'Visa Classic 1435442169918409'}
-    data_after_test = 'Visa Classic **8409'
+    desired_result = 'Visa Classic **8409'
 
-    assert functions.beneficiary_account_editor(testing_data) == data_after_test
+    assert functions.beneficiary_account_editor(testing_data) == desired_result
